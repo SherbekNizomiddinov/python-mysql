3d716b36f8c6a614b5db0f944c45805084ab7ed8
SherbekNizomiddinov <sherbekn068@gmail.com>
2025-02-07 10:41:49 +0500

    Done

--- main.py
+++ main.py
@@ -1,24 +1,82 @@
-import mysql.connector
-import settings
+from mysql.connector import connect
 from db import (
     create_books_table,
     insert_book,
     show_all_books,
+    search_books_by_author_or_genre,
+    update_book_price,
+    update_book_availability,
+    delete_book,
+    sort_books_by_year,
+    count_books,
+    price_statistics
 )
 
-
-if __name__ == "__main__":
-    connection = mysql.connector.connect(
-        host=settings.host,
-        user=settings.user,
-        password=settings.password,
-        port=settings.port
+    # MySQL ga bog'lanish
+c = connect(
+        host =       "localhost",
+        user =       "root",
+        password =   "Salom_006",
+        port =       "3306",
+        database =   "company_db"
     )
+cursor = c.cursor()
+    # Shu narsani CHATGPT orqali bilib oldim bu kodni yozmasim oldin har gal ma'lumotni qayta qushib ketdi
+# cursor.execute("TRUNCATE TABLE books;")
+    # Table yaratish funkiyasi
+create_books_table(cursor)
 
-    cursor = connection.cursor()
 
-    # db dagi functionni ishlating
+    # kitob qushish
+b = [
+    ("Atomic Habits", "James Clear", 2018, "Self-help", 15.99, True),
+    ("Deep Work", "Cal Newport", 2016, "Productivity", 12.50, True),
+    ("Python Crash Course", "Eric Matthes", 2019, "Programming", 22.95, True)
+]
+insert_book(cursor, b)
+    # tableni ma'lumotlarini saqlash uchun keral
+c.commit() 
 
-    # close
-    cursor.close()
-    connection.close()
+
+    # jadvalni chiqarish
+show_all_books(cursor)
+
+
+    # Ma'lum bir shart bo'yicha qidiruv
+d = input("'author' yoki 'gender' kiriting --> " ).strip().lower()
+r = input("qidirayotgan narsangizning nomini kiriting --> " ).strip()
+search_books_by_author_or_genre(cursor, d, r)
+
+
+    # kitobni narxini yangilash
+book_id = int(input("Narxini yangilamoqchi bulgan kitob id raqamini kiriting --> " ))
+new_price = float(input("Yangi narxni kiriting floatda bulsa yaxshi --> " ))
+update_book_price(cursor, new_price, book_id)
+
+
+    # Kitob mavjudligini o'zgartirish
+a = int(input("mavjudligini uzgartirmoqchi bulgan kitobni id raqamini kiriting --> " ))
+b = int(input("availableni 'True = 1', 'False = 0' raqami buyicha uzgartiring --> " ))
+update_book_availability(cursor, a, b)
+
+
+    # Kitobni uchirish
+a = int(input("Uchirmoqchi bulgan narsangizni id raqamini kiritng --> " ))
+delete_book(cursor, a)
+
+
+    # Yil bo'yicha saralash
+sort = int(input("1- raqam usish taribida, 2- raqam kamayish tartibida --> ")) 
+sort_books_by_year(cursor, sort)
+
+
+    # Jami kitoblar sonini chiqarish
+count_books(cursor)
+
+
+    # Narx bo'yicha statistikani chiqarish
+price_statistics(cursor)
+
+
+cursor.close()
+c.close()
